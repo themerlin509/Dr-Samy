ba10787f57d0a25e7eb72885b3b868c1a2f440e6
themerlin <robertoleblanc10@gmail.com>
2025-11-05 22:56:23 -0500

    Update geminiService.ts 1
    API KEY

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -8,7 +8,7 @@ if (!API_KEY) {
   throw new Error("API_KEY environment variable is not set.");
 }
 
-const ai = new GoogleGenAI({ apiKey: API_KEY });
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_API_KEY });
 
 const safetySettings = [
   {
